44aeb810633a9ec910a4159b0aacfbad56d505da
katekanina <katekanina@gmail.com>
2018-03-31 13:43:50 +0300

    adding !

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $_SESSION['user_id'] = 1;
 require (MYSQL);
 include ('./includes/header.html');
 
-?><h3>Welcome</h3>
+?><h3>Welcome!</h3>
 	<p class="lead">Welcome to Knowledge is Power, but it's not always true
 	a site dedicated to keeping you up to date on the Web security 
 	and programming information you need to know. Blah, blah, blah. Yadda, yadda, yadda.</p>
